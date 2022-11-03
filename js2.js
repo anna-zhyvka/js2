e9cfff1e9dae77aa06602c8264df52378d30bdd4
anna-zhyvka <annazhyvka516@gmail.com>
2022-11-03 12:55:50 +0200

    Initial commit

---
+++ js2.js
@@ -0,0 +1,33 @@
+"use strict";
+//1task
+let a = [300, 'abc', 'idea', false, 1, [5,6,7], 'game', 'number',29900, true];
+console.log(a);
+//2task
+let book1 = { title: 'HarryPotter', pageCount: 500, genre: 'fantasy'};
+let book2 = { title: 'TheLordOfTheRings',pageCount: 400,genre: 'fantasy'};
+let book3 = { title: 'Twilight', pageCount:460, genre: 'fantasy'};
+//3task
+let book4 = {title: 'HarryPotter', pageCount: 500, genre: 'fantasy', authors: ['Rowling', 57]};
+let book5 = { title:'TheLordOfTheRings',pageCount: 400,genre: 'fantasy', authors: ['Tolkien', 81]};
+let book6 = { title:'Twilight', pageCount:460, genre: 'fantasy', authors: ['Meyer', 48]};
+//4task
+let user = [{name: 'Anna', username:'qwer',password: '5690'},
+    {name: 'Bella', username: 'uiop', password: 'lkjhg'},
+    {name: 'Antony', username: 'qqqqq', password: '1023'},
+    {name: 'Katya', username: 'katherine', password:'OOOO'},
+    {name: 'Jake', username: 'jjjj', password: 'jk34'},
+    {name: 'George', username: 'oooo', password: 'llll'},
+    {name: 'Olya', username: 'uiop', password: 'vbnmm'},
+    {name: 'Mike', username: 'ryeu', password: 'cjsw'},
+    {name: 'Claus', username: 'bsmek', password: 'yesbk'},
+    {name: 'Tina', username: 'sclslo', password: 'dkksnl'}  ]
+console.log(user[0].password);
+console.log(user[1].password);
+console.log(user[2].password);
+console.log(user[3].password);
+console.log(user[4].password);
+console.log(user[5].password);
+console.log(user[6].password);
+console.log(user[7].password);
+console.log(user[8].password);
+console.log(user[9].password);
